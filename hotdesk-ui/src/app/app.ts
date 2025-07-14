4f8b342b7df122a96d215c91b02183ca546b9a8a
AinurImal <imalainur@gmail.com>
2025-07-14 16:32:49 +0800

    Added my first angular hotdesk-ui

--- hotdesk-ui/src/app/app.ts
+++ hotdesk-ui/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Home } from './home/home'; // <-- Import your Home component
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterModule, RouterOutlet, Home], // <-- Add Home here
   templateUrl: './app.html',
-  standalone: false,
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'hotdesk-ui';
 }
+
---
+++ hotdesk-ui/src/app/home/home.ts
@@ -0,0 +1,22 @@
+/*import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}*/
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home { }
