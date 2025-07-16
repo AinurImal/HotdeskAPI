ce52eed767c1d9e126eeb59bc779f372e6315743
AinurImal <imalainur@gmail.com>
2025-07-16 15:29:00 +0800

    Add new components and pages to hotdesk ui

--- hotdesk-ui/src/app/booking/booking.ts
+++ hotdesk-ui/src/app/booking/booking.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-booking',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './booking.html',
   styleUrl: './booking.css'
 })
-export class Booking {
-
-}
+export class Booking { }
---
+++ hotdesk-ui/src/app/desk/desk.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desk',
+  imports: [],
+  templateUrl: './desk.html',
+  styleUrl: './desk.css'
+})
+export class Desk {
+
+}
