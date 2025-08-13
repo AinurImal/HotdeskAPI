98e034cf9b2a13cf93b07ee2df3c7abea65bbea1
Ainur Imal <Ainur Imal@USER>
2025-08-13 19:45:48 +0800

    Minor adjustment to set up current code in new environment

--- HotdeskAPI/Program.cs
+++ HotdeskAPI/Program.cs
@@ -1,18 +1,41 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using HotdeskAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HotdeskAPIContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("HotdeskAPIContext") ?? throw new InvalidOperationException("Connection string 'HotdeskAPIContext' not found.")));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Auto-create database and apply migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<HotdeskAPIContext>();
+    try
+    {
+        // Ensure database is created and migrations are applied
+        context.Database.EnsureCreated();
+        
+        // Apply any pending migrations
+        if (context.Database.GetPendingMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+    }
+    catch (Exception ex)
+    {
+        // Log the error but don't stop the application
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while creating/migrating the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
